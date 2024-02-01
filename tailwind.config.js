d2e0f93f51828c64489038d8aa86f549ac2522e5
roelh <roelhoogendoorn01369@gmail.com>
2024-02-01 17:29:52 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/index.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
