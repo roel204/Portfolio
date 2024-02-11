275cd3017c2510f6e624a02bad91ea0c9f8a6053
roelh <roelhoogendoorn01369@gmail.com>
2024-02-11 15:16:28 +0100

    Added language capability to ProjectsPage.jsx

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -47,22 +47,70 @@ const ProjectsPage = () => {
     const texts = {
         en: {
             pageTitle: "My Projects",
+            code: "View Code",
         },
         nl: {
             pageTitle: 'Mijn Projecten',
+            code: 'Bekijk Code',
         },
     };
 
     const projects = [
-        {title: 'Project 1', shortDesc: 'shortDesc of Project 1', longDesc: 'shortDesc of Project 1', githubLink: 'https://github.com/project1'},
-        {title: 'Project 2', shortDesc: 'shortDesc of Project 2', longDesc: 'shortDesc of Project 2', githubLink: 'https://github.com/project2'},
-        {title: 'Project 3', shortDesc: 'shortDesc of Project 3', longDesc: 'shortDesc of Project 3', githubLink: 'https://github.com/project3'},
-        {title: 'Project 4', shortDesc: 'shortDesc of Project 4', longDesc: 'shortDesc of Project 4', githubLink: 'https://github.com/project4'},
-        {title: 'Project 5', shortDesc: 'shortDesc of Project 5', longDesc: 'shortDesc of Project 5', githubLink: 'https://github.com/project5'},
-        {title: 'Project 6', shortDesc: 'shortDesc of Project 6', longDesc: 'shortDesc of Project 6', githubLink: 'https://github.com/project6'},
-        {title: 'Project 7', shortDesc: 'shortDesc of Project 7', longDesc: 'shortDesc of Project 7', githubLink: 'https://github.com/project7'},
-        {title: 'Project 8', shortDesc: 'shortDesc of Project 8', longDesc: 'shortDesc of Project 8', githubLink: 'https://github.com/project8'},
-        {title: 'Project 9', shortDesc: 'shortDesc of Project 9', longDesc: 'shortDesc of Project 9', githubLink: 'https://github.com/project9'},
+        {
+            title: 'Project 1', githubLink: 'https://github.com/project1',
+            shortDesc: {
+                en: 'shortDesc of Project 1',
+                nl: 'nl',
+            },
+            longDesc: {
+                en: 'longDesc of Project 1',
+                nl: '',
+            },
+        },
+        {
+            title: 'Project 1', githubLink: 'https://github.com/project1',
+            shortDesc: {
+                en: 'shortDesc of Project 1',
+                nl: 'nl',
+            },
+            longDesc: {
+                en: 'longDesc of Project 1',
+                nl: '',
+            },
+        },
+        {
+            title: 'Project 1', githubLink: 'https://github.com/project1',
+            shortDesc: {
+                en: 'shortDesc of Project 1',
+                nl: 'nl',
+            },
+            longDesc: {
+                en: 'longDesc of Project 1',
+                nl: '',
+            },
+        },
+        {
+            title: 'Project 1', githubLink: 'https://github.com/project1',
+            shortDesc: {
+                en: 'shortDesc of Project 1',
+                nl: 'nl',
+            },
+            longDesc: {
+                en: 'longDesc of Project 1',
+                nl: '',
+            },
+        },
+        {
+            title: 'Project 1', githubLink: 'https://github.com/project1',
+            shortDesc: {
+                en: 'shortDesc of Project 1',
+                nl: 'nl',
+            },
+            longDesc: {
+                en: 'longDesc of Project 1',
+                nl: '',
+            },
+        },
     ];
 
     return (
@@ -82,11 +130,11 @@ const ProjectsPage = () => {
                                 <path
                                     d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
                             </svg>
-                            <span className="">View Code</span>
+                            <span className="">{texts[language].code}</span>
                         </a>
 
                         {/* shortDesc */}
-                        <p className="mt-8 text-sm">{project.shortDesc}</p>
+                        <p className="mt-8 text-sm">{project.shortDesc[language]}</p>
 
                         {/* Dropdown Arrow */}
                         <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2">
@@ -130,7 +178,7 @@ const ProjectsPage = () => {
                     However, when we use diagrams, maps or graphs, the ALT text is not usually enough to provide an adequate text alternative. To present a long shortDesc, we might need to use
                     headings, dot points or data tables. While ALT text can be long (there's no technical limit), long text usually needs structure. And ALT text can only be text.</p>
             </div>
-            
+
             <div className="px-20 py-10 border-b border-cgreen">
                 <h3 className="text-5xl text-cgreen">Title 1</h3>
                 <p>Long shortDescs are text versions of the information provided in a detailed or complex image.
