0de91c3d5e5cf4a3570e3351afa1c65f98e31755
roelh <roelhoogendoorn01369@gmail.com>
2024-02-21 16:31:29 +0100

    Change to router, will now hopefully work on GH Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
-import {LanguageProvider} from './LanguageContext';
-import Navbar from './Navbar.jsx';
-import HomePage from './HomePage.jsx';
-import ProjectsPage from './ProjectsPage.jsx';
-import SkillsPage from './SkillsPage.jsx';
+import {Outlet} from "react-router-dom";
 
 function App() {
     return (
-        <Router>
-            <LanguageProvider>
-                <Navbar/>
-                <Routes>
-                    <Route path="/Portfolio" element={<HomePage/>}/>
-                    <Route path="/Portfolio/projects" element={<ProjectsPage/>}/>
-                    <Route path="/Portfolio/skills" element={<SkillsPage/>}/>
-                </Routes>
-            </LanguageProvider>
-        </Router>
+        <Outlet />
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
+import App from './App.jsx'
+import HomePage from "./HomePage.jsx";
+import ProjectsPage from "./ProjectsPage.jsx";
+import SkillsPage from "./SkillsPage.jsx";
+import Navbar from "./Navbar.jsx";
+import {LanguageProvider} from "./LanguageContext.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/Portfolio/",
+        element: (
+            <LanguageProvider>
+                <Navbar />
+                <App />
+            </LanguageProvider>
+        ),
+        children: [
+            {
+                path: "/Portfolio/",
+                element: <HomePage />,
+            },
+            {
+                path: "/Portfolio/projects",
+                element: <ProjectsPage />,
+            },
+            {
+                path: "/Portfolio/skills",
+                element: <SkillsPage />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
