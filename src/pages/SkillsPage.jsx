465c23e19f76bc34194f9728f485520a14ed1c8a
roelh <roelhoogendoorn01369@gmail.com>
2024-03-13 19:47:41 +0100

    Build a template for PC building section

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -21,7 +21,7 @@ const SkillsPage = () => {
 
     return (
         <div className="sm:ml-[6vw] mt-[5vh] flex flex-col justify-center items-center max-w-full min-h-screen">
-            <div className="py-10 sm:px-10 bg-green-50/75 rounded-3xl dark:bg-black/75">
+            <div className="py-10 sm:px-10 sm:w-[50vw] bg-green-50/75 rounded-3xl dark:bg-black/75">
                 <h1 className="gradientText text-5xl sm:text-8xl pb-5">{texts[language].codeTitle}</h1>
                 <p className="text-gray-400 text-center px-4 sm:whitespace-pre-line">{texts[language].codeDesc}</p>
 
@@ -137,9 +137,151 @@ const SkillsPage = () => {
                 </div>
             </div>
 
-            <div className="py-10 mt-[5vh] sm:px-10 bg-green-50/75 rounded-3xl dark:bg-black/75">
+            <div className="py-10 my-[5vh] sm:px-10 sm:w-[50vw] bg-green-50/75 rounded-3xl dark:bg-black/75">
                 <h1 className="gradientText text-5xl sm:text-8xl pb-5">{texts[language].pcTitle}</h1>
                 <p className="text-gray-400 text-center px-4 sm:whitespace-pre-line">{texts[language].pcDesc}</p>
+
+                {/*My Computer*/}
+                <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
+                    <h3 className="text-2xl font-bold">My first computer</h3>
+                </div>
+                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="flex flex-col justify-around">
+                        <div className="p-2">
+                            <p>This is the first pc i built myself. I took a long time saving up for all the parts and built it together with a friend.</p>
+                        </div>
+                        <div className="flex">
+                            <div className="p-2 pr-5 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>CPU:</p>
+                                    <p>GPU:</p>
+                                </div>
+                                <div>
+                                    <p>Ryzen 5 2600X</p>
+                                    <p>RX590</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>RAM:</p>
+                                    <p>MOBO:</p>
+                                </div>
+                                <div>
+                                    <p>Team Group Delta 16GB</p>
+                                    <p>Asrock B450 Pro4</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>PSU:</p>
+                                    <p>CASE:</p>
+                                </div>
+                                <div>
+                                    <p>CM MWE Gold 650</p>
+                                    <p>Antec NX600</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    </div>
+                </div>
+
+                {/*Jitse Computer*/}
+                <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
+                    <h3 className="text-2xl font-bold">Computer i built for my little brother</h3>
+                </div>
+                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="flex flex-col justify-around">
+                        <div className="p-2">
+                            <p>My little brother wanted a computer, so i made a simple integrated graphics build, he later bought a RX6600 for me to install.</p>
+                        </div>
+                        <div className="flex">
+                            <div className="p-2 pr-5 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>CPU:</p>
+                                    <p>GPU:</p>
+                                </div>
+                                <div>
+                                    <p>Ryzen 5 3400G</p>
+                                    <p>Asrock RX6600</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>RAM:</p>
+                                    <p>MOBO:</p>
+                                </div>
+                                <div>
+                                    <p>Team Group Delta 16GB</p>
+                                    <p>Asrock B450M Pro4</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>PSU:</p>
+                                    <p>CASE:</p>
+                                </div>
+                                <div>
+                                    <p>EVGA Gold 450</p>
+                                    <p>Aerocool Hive</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    </div>
+                </div>
+
+                {/*Vincent Computer*/}
+                <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
+                    <h3 className="text-2xl font-bold">Computer i built for my other little brother</h3>
+                </div>
+                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="flex flex-col justify-around">
+                        <div className="p-2">
+                            <p>My other little brother also wanted a computer, so he took a loan and i build one for him.</p>
+                        </div>
+                        <div className="flex">
+                            <div className="p-2 pr-5 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>CPU:</p>
+                                    <p>GPU:</p>
+                                </div>
+                                <div>
+                                    <p>Ryzen 5 5500</p>
+                                    <p>Asrock RX6600</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>RAM:</p>
+                                    <p>MOBO:</p>
+                                </div>
+                                <div>
+                                    <p>Corsair Vengeance 8gb</p>
+                                    <p>Asus Prime B550M-K</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex">
+                                <div className="pr-2 text-gray-400">
+                                    <p>PSU:</p>
+                                    <p>CASE:</p>
+                                </div>
+                                <div>
+                                    <p>Seasonic G12 GM-650</p>
+                                    <p>MSI MAG Forge M100R</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
