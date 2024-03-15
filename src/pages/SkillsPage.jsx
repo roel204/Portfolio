41839da8325afde3ec9aeda7a7c1dc65f00ed3f8
roelh <roelhoogendoorn01369@gmail.com>
2024-03-15 12:57:51 +0100

    Finished Computer part

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -9,13 +9,26 @@ const SkillsPage = () => {
             codeTitle: "CODE SKILLS",
             codeDesc: "I have learned multiple programming languages, frameworks and other programs.\nI have learned most of these from my study, but some are also self-taught because it's interesting to me.",
             pcTitle: "PC BUILDING",
-            pcDesc: "I have built multiple PC's..."
+            pcDesc: "I have built multiple computers and love searching for the perfect parts that will fit the build.",
+            roelTitle: "My Computer",
+            roelDesc: "I built this computer together with a friend. I looked at a lot of tutorials and searched for all the best parts to buy.",
+            jitseTitle: "My little brother Jitse's PC",
+            jitseDesc: "My little brother wanted a computer, so I made a simple integrated graphics build. He later bought an RX6600 for me to install.",
+            vincentTitle: "My little brother Vincent's PC",
+            vincentDesc: "My other little brother also wanted a computer, so he took a loan and I built one for him."
         },
         nl: {
             codeTitle: 'CODE SKILLS',
             codeDesc: "Ik heb meerdere programmeertalen, frameworks en andere programma's geleerd.\nDe meeste heb ik tijdens mijn studie geleerd, maar sommige heb ik zelf geleerd omdat ik het interessant vind.",
             pcTitle: "PC BOUWEN",
-            pcDesc: "Ik heb meerdere PC's in elkaar gezet..."
+            pcDesc: "Ik heb meerdere computers gebouwd en vind het leuk om op zoek te gaan naar de perfecte onderdelen die bij de computer passen.",
+            roelTitle: "Mijn Computer",
+            roelDesc: "Ik heb deze computer samen met een vriend gebouwd. Ik heb naar veel tutorials gekeken en gezocht naar de beste onderdelen om te kopen.",
+            jitseTitle: "De PC voor mijn broertje Jitse",
+            jitseDesc: "Mijn broertje wilde een computer, dus heb ik een eenvoudige build met geïntegreerde graphics gemaakt. Later kocht hij een RX6600 voor mij om te installeren.",
+            vincentTitle: "De PC voor mijn broertje Vincent",
+            vincentDesc: "Mijn andere broertje wilde ook een computer, dus hij nam een lening en ik bouwde er een voor hem."
+
         },
     };
 
@@ -141,144 +154,72 @@ const SkillsPage = () => {
                 <h1 className="gradientText text-5xl sm:text-8xl pb-5">{texts[language].pcTitle}</h1>
                 <p className="text-gray-400 text-center px-4 sm:whitespace-pre-line">{texts[language].pcDesc}</p>
 
-                {/*My Computer*/}
+                {/*Roel Computer*/}
                 <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
-                    <h3 className="text-2xl font-bold">My first computer</h3>
+                    <h3 className="text-2xl font-bold">{texts[language].roelTitle}</h3>
                 </div>
-                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
-                    <div className="flex flex-col justify-around">
+                <div className="flex justify-between border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="borr-2 flex flex-col justify-around w-[60%]">
                         <div className="p-2">
-                            <p>This is the first pc i built myself. I took a long time saving up for all the parts and built it together with a friend.</p>
+                            <p>{texts[language].roelDesc}</p>
                         </div>
-                        <div className="flex">
-                            <div className="p-2 pr-5 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>CPU:</p>
-                                    <p>GPU:</p>
-                                </div>
-                                <div>
-                                    <p>Ryzen 5 2600X</p>
-                                    <p>RX590</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>RAM:</p>
-                                    <p>MOBO:</p>
-                                </div>
-                                <div>
-                                    <p>Team Group Delta 16GB</p>
-                                    <p>Asrock B450 Pro4</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>PSU:</p>
-                                    <p>CASE:</p>
-                                </div>
-                                <div>
-                                    <p>CM MWE Gold 650</p>
-                                    <p>Antec NX600</p>
-                                </div>
-                            </div>
+                        <div className="p-2 text-sm sm:columns-2">
+                            <p><span className="text-gray-400">CPU:</span> Ryzen 5 2600X</p>
+                            <p><span className="text-gray-400">GPU:</span> Gigabyte RX590</p>
+                            <p><span className="text-gray-400">RAM:</span> Team Group Delta 16GB</p>
+                            <p><span className="text-gray-400">MOBO:</span> Asrock B450 Pro4</p>
+                            <p><span className="text-gray-400">PSU:</span> CoolerM MWE Gold 650</p>
+                            <p><span className="text-gray-400">CASE:</span> Antec NX600</p>
                         </div>
                     </div>
-                    <div className="p-2">
-                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    <div className="flex w-[40%]">
+                        <img src="public/images/roelPCsm.png" alt="Vincent's PC" className="sm:max-w-[10vw] sm:mr-5 ml-auto object-contain transition sm:hover:scale-125"/>
                     </div>
                 </div>
 
                 {/*Jitse Computer*/}
                 <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
-                    <h3 className="text-2xl font-bold">Computer i built for my little brother</h3>
+                    <h3 className="text-2xl font-bold">{texts[language].jitseTitle}</h3>
                 </div>
-                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
-                    <div className="flex flex-col justify-around">
+                <div className="flex justify-between border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="flex flex-col justify-around w-[60%]">
                         <div className="p-2">
-                            <p>My little brother wanted a computer, so i made a simple integrated graphics build, he later bought a RX6600 for me to install.</p>
+                            <p>{texts[language].jitseDesc}</p>
                         </div>
-                        <div className="flex">
-                            <div className="p-2 pr-5 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>CPU:</p>
-                                    <p>GPU:</p>
-                                </div>
-                                <div>
-                                    <p>Ryzen 5 3400G</p>
-                                    <p>Asrock RX6600</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>RAM:</p>
-                                    <p>MOBO:</p>
-                                </div>
-                                <div>
-                                    <p>Team Group Delta 16GB</p>
-                                    <p>Asrock B450M Pro4</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>PSU:</p>
-                                    <p>CASE:</p>
-                                </div>
-                                <div>
-                                    <p>EVGA Gold 450</p>
-                                    <p>Aerocool Hive</p>
-                                </div>
-                            </div>
+                        <div className="p-2 text-sm sm:columns-2">
+                            <p><span className="text-gray-400">CPU:</span> Ryzen 5 3400G</p>
+                            <p><span className="text-gray-400">GPU:</span> Asrock RX6600</p>
+                            <p><span className="text-gray-400">RAM:</span> Team Group Delta 16GB</p>
+                            <p><span className="text-gray-400">MOBO:</span> Asrock B450M Pro4</p>
+                            <p><span className="text-gray-400">PSU:</span> EVGA Gold 450</p>
+                            <p><span className="text-gray-400">CASE:</span> Aerocool Hive</p>
                         </div>
                     </div>
-                    <div className="p-2">
-                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    <div className="flex w-[40%]">
+                        <img src="public/images/jitsePCsm.png" alt="Vincent's PC" className="sm:max-w-[10vw] sm:mr-5 ml-auto object-contain transition sm:hover:scale-125"/>
                     </div>
                 </div>
 
                 {/*Vincent Computer*/}
                 <div className="border-2 border-cgreen rounded-t-2xl p-4 mt-[5vh] text-center bg-black/[.5]">
-                    <h3 className="text-2xl font-bold">Computer i built for my other little brother</h3>
+                    <h3 className="text-2xl font-bold">{texts[language].vincentTitle}</h3>
                 </div>
-                <div className="flex justify-around border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
-                    <div className="flex flex-col justify-around">
+                <div className="flex justify-between border-x-2 border-b-2 border-cgreen rounded-b-2xl p-2">
+                    <div className="flex flex-col justify-around w-[60%]">
                         <div className="p-2">
-                            <p>My other little brother also wanted a computer, so he took a loan and i build one for him.</p>
+                            <p>{texts[language].vincentDesc}</p>
                         </div>
-                        <div className="flex">
-                            <div className="p-2 pr-5 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>CPU:</p>
-                                    <p>GPU:</p>
-                                </div>
-                                <div>
-                                    <p>Ryzen 5 5500</p>
-                                    <p>Asrock RX6600</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>RAM:</p>
-                                    <p>MOBO:</p>
-                                </div>
-                                <div>
-                                    <p>Corsair Vengeance 8gb</p>
-                                    <p>Asus Prime B550M-K</p>
-                                </div>
-                            </div>
-                            <div className="p-2 flex">
-                                <div className="pr-2 text-gray-400">
-                                    <p>PSU:</p>
-                                    <p>CASE:</p>
-                                </div>
-                                <div>
-                                    <p>Seasonic G12 GM-650</p>
-                                    <p>MSI MAG Forge M100R</p>
-                                </div>
-                            </div>
+                        <div className="p-2 text-sm sm:columns-2">
+                                <p><span className="text-gray-400">CPU:</span> Ryzen 5 5500</p>
+                                <p><span className="text-gray-400">GPU:</span> Asrock RX6600</p>
+                                <p><span className="text-gray-400">RAM:</span> Corsair Vengeance 8gb</p>
+                                <p><span className="text-gray-400">MOBO:</span> Asus Prime B550M-K</p>
+                                <p><span className="text-gray-400">PSU:</span> Seasonic G12 GM-650</p>
+                                <p><span className="text-gray-400">CASE:</span> MSI MAG Forge M100R</p>
                         </div>
                     </div>
-                    <div className="p-2">
-                        <img src="public/images/logos/mongodb.png" alt="placeholder" className="w-20 h-20 sm:w-[10vw] sm:h-[18vh] object-contain"/>
+                    <div className="flex w-[40%]">
+                        <img src="public/images/vincentPCsm.png" alt="Vincent's PC" className="sm:max-w-[10vw] sm:mr-5 ml-auto object-contain transition sm:hover:scale-125"/>
                     </div>
                 </div>
 
