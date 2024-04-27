6f9a32ce95c713852eb615dbd4c0d6c8f07da2cd
Roel <roelhoogendoorn01369@gmail.com>
2024-04-27 16:18:46 +0200

    Removed hennie and fixed flex text

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -60,7 +60,6 @@ const ProjectsPage = () => {
             streamTeam: "Streamteam is a Tailored Learning Environment (TLE) project for the CMGT study at Rotterdam University of Applied Sciences. The project focuses on the existing IP-car, a car controlled via a server with a live feed, which is intended for use in daily activities within care homes. Through this software, the IP-car can connect to a guide who can see a live feed and take control of the car with a controller via the same connection. It is a delivery to VindiQu and FoxConnect. VindiQu already offers livestream daily activities to care homes, and FoxConnect is the developer of the first IP-car, originally intended for private use by people with mobility impairments.",
             pcShowcase: "This is my first school project using the Laravel framework. I used it to make a simple CRUD application where people can post their PC's. Users can also place comments on a Computer and talk about it. I used Bootstrap for the CSS in this website.",
             hungryFish: "First i made Hungry Fish as an Excalibur project for school. But later i recreated it with unity and improved it, adding way more features like upgrades and skins. The Unity game is still in development.",
-            hennie: "I made this CLE3 project with other students. We used JavaScript to build a web-app that displays information for elderly people. I also added a favorites system using Localstorage.",
         },
         nl: {
             pageTitle: 'MIJN PROJECTEN',
@@ -69,7 +68,6 @@ const ProjectsPage = () => {
             streamTeam: "Streamteam is een Tailored Learning Environment (TLE) project voor de studie CMGT, Hogeschool Rotterdam. Het project focust op de bestaande IP-car, een via een server te besturen auto met een livefeed, die moet worden ingezet voor dagbesteding binnen zorghuizen. De IP-car kan via deze software verbinden met een gids, die een live beeld te zien krijgt en controle van de auto kan overnemen met een controller via dezelfde verbinding. Het is een oplevering aan VindiQu en FoxConnect. VindiQu biedt al livestream dagbesteding aan zorghuizen, en FoxConnect is de ontwikkelaar van de eerste IP-car, origineel bedoeld voor particulier gebruik van mensen met een motorische beperking.",
             pcShowcase: "Dit is mijn eerste school project waarbij ik het Laravel-framework gebruik. Ik heb het gebruikt om een eenvoudige CRUD-applicatie te maken waar mensen hun pc's kunnen plaatsen. Gebruikers kunnen ook opmerkingen plaatsen over een computer en erover praten. Ik heb Bootstrap gebruikt voor de CSS op deze website.",
             hungryFish: "Eerst heb ik Hungry Fish gemaakt als een Excalibur-project voor school. Maar later heb ik het opnieuw gemaakt met Unity en verbeterd, waarbij ik veel meer functies heb toegevoegd zoals upgrades en skins. De Unity game is nog in ontwikkeling.",
-            hennie: "Ik heb dit CLE3-project gemaakt met andere studenten. We hebben JavaScript gebruikt om een web-app te bouwen die informatie weergeeft voor ouderen. Ik heb ook een favorietensysteem toegevoegd met behulp van LocalStorage.",
         },
     };
 
@@ -112,14 +110,6 @@ const ProjectsPage = () => {
             desc: {
                 en: 'A simple fish game made with Unity.',
                 nl: 'Een game over vissen gemaakt met Unity.',
-            },
-        },
-        {
-            title: "Hennie's Hulp Pagina",
-            tags: 'CLE3 JS',
-            desc: {
-                en: 'A help page for elderly people.',
-                nl: 'Een hulp pagina voor senioren.',
             },
         },
     ];
@@ -191,7 +181,7 @@ const ProjectsPage = () => {
                     </div>
 
                     {/* Description */}
-                    <p className="text-sm xl:text-lg">{texts[language].spellAI}</p>
+                    <p className="text-sm xl:text-lg">{texts[language].flexCounter}</p>
                 </div>
 
                 <div id="1" className="py-10 my-6 px-5 xl:px-10 xl:w-[60vw] relative rounded-3xl bg-green-50/75 dark:bg-black/75">
@@ -292,27 +282,6 @@ const ProjectsPage = () => {
 
                     {/* Description */}
                     <p className="text-sm xl:text-lg">{texts[language].hungryFish}</p>
-                </div>
-
-                <div id="5" className="py-10 my-6 px-5 xl:px-10 xl:w-[60vw] relative rounded-3xl bg-green-50/75 dark:bg-black/75">
-
-                    {/* Project Title */}
-                    <h3 className="text-2xl xl:text-5xl font-bold text-cgreen mb-8">Hennie's Hulp Pagina</h3>
-
-                    {/* Links */}
-                    <div className="flex flex-row absolute top-5 right-5">
-                        <a href="https://github.com/roel204/CLE3" target="_blank" className="flex flex-col items-center p-3 rounded-lg hover:bg-dgreen dark:hover:bg-cgreen group">
-                            <svg className="w-10 h-10 text-gray-700 dark:text-gray-300 group-hover:text-gray-900 dark:group-hover:text-white group-hover:scale-110" aria-hidden="true"
-                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 496 512">
-                                <path
-                                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-                            </svg>
-                            <span className="">Code</span>
-                        </a>
-                    </div>
-
-                    {/* Description */}
-                    <p className="text-sm xl:text-lg">{texts[language].hennie}</p>
                 </div>
             </div>
         </div>
